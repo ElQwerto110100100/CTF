b8ad2a2e334f74a4f36d46e5db2dfcccf4c46fbc
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2019-04-16 19:17:09 +1000

    new challenge

---
+++ pico2018/quackme/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <limits.h>
+
+int main()
+{
+  char charcter = 'a' + 'a';
+  printf("%c\n",charcter );
+  return 0;
+}
